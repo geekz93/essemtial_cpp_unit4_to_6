f75b8541e948d9627281476bcdae52cdaf566bb2
zooo <imzooo@163.com>
2016-06-29 14:15:33 +0800

    练习4.3

---
+++ class_prac.h
@@ -0,0 +1,16 @@
+#ifndef _CLASS_PRAC
+#define _CLASS_PRAC
+#include <string>
+using namespace std;
+class Program
+{
+public:
+private:
+	string program_name;
+	string version_stamp;
+	int version_number;
+	int tests_run;
+	int tests_passed;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "class_prac.h"
+int main()
+{
+
+	return 0;
+}
